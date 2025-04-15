87ccc8a089573ea0a5839d2e79ec8c4a99807b4d
Miguel <miguelpocas2007@gmail.com>
2025-04-15 14:18:10 -0300

    Add files via upload

---
+++ ClasseEObjetosALUNOS.js
@@ -0,0 +1,18 @@
+class alunos {
+    nome;
+    idade;
+    curso;
+}
+
+var aluno1 = new alunos();
+aluno1.nome = "Miguel"
+aluno1.idade = 18
+aluno1.curso = "Back-end"
+
+var aluno2 = new alunos();
+aluno2.nome = "Arthur"
+aluno2.idade = 18
+aluno2.curso = "Back-end"
+
+console.log(aluno1)
+console.log(aluno2)
