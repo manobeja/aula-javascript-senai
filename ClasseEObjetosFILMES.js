8a659862064dc063d2fac5412cf4ae87c81cd7c9
Miguel <miguelpocas2007@gmail.com>
2025-04-15 14:46:54 -0300

    Add files via upload

---
+++ ClasseEObjetosFILMES.js
@@ -0,0 +1,15 @@
+class filme {
+    titulo;
+    genero;
+}
+
+var filme1 = new filme()
+filme1.titulo = "De Volta Para o Futuro"
+filme1.genero = "Ficção Científica"
+
+var filme2 = new filme()
+filme2.titulo = "De Volta Para o Futuro 2"
+filme2.genero = "Ficção Científica"
+
+console.log(filme1.genero)
+console.log(filme2.genero)
