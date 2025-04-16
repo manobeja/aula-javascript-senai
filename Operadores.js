6e22f6d8ec10fa127056d62abb6f47ac3a5833c1
Miguel <miguelpocas2007@gmail.com>
2025-04-16 13:09:42 -0300

    Add files via upload

---
+++ Operadores.js
@@ -0,0 +1,19 @@
+console.log(5 == 5)                   // Um número igual ao outro
+
+console.log(5 != 5)                   // Diferença de valor
+
+console.log(10 > 7)                   // Maior que
+
+console.log(3 < 5)                    // Menos que
+
+console.log(7 >= 7)                   // Maior ou igual 
+
+console.log(4 <= 9)                   // Menor ou igual
+
+console.log(5 == 5 && 10 > 7)         // Um e o outro
+
+console.log(5 == 4 && 10 > 7)         // Um e o outro
+
+console.log(5 == 5 || 5 > 7)          // Um ou o outro
+
+console.log(5 == 4 || 5 > 7)          // Um ou o outro
