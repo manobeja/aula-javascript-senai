7e9eb28caee8407b07ee4a731117623075963846
Miguel <miguelpocas2007@gmail.com>
2025-05-12 15:06:13 -0300

    Add files via upload

--- localhost.js
+++ localhost.js
@@ -6,5 +6,20 @@ app.get("/", (req, res) => {
     res.send("Servidor Express Funcionando!");
 })
 
+app.get("/sobre", (req, res) => {
+    res.send("Sobre nós!")
+})
+
+app.get("/usuario", (req, res) => {
+    const usuario = "Miguel"
+    res.send(usuario)
+})
+
+app.get("/senai", (req, res) => {
+    const curso = "Back-end"
+    res.send(curso)
+})
+
 app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000")})
+    console.log("Servidor rodando em http://localhost:3000")
+})
