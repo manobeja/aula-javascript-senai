5ea9a158df29219eec31ee69f7fd3a3c0842b063
Miguel <miguelpocas2007@gmail.com>
2025-05-14 16:43:59 -0300

    Add files via upload

---
+++ objetocelularapi.js
@@ -0,0 +1,8 @@
+let celulares = [
+    { marca: "Motorola", processador: "Intel 3", ano: 2023, tela: "6 polegadas", modelo: "Motog 8"}, 
+    { marca: "Iphone", processador: "Intel 1", ano: 2024, tela: "3 polegadas", modelo: "Iphone 15"  },
+    { marca: "Sansung", processador: "Intel 2", ano: 2022, tela: "5 polegadas", modelo: "A6" }
+]
+
+console.log(celulares[1].processador);
+console.log(celulares[0].tela);
